0c85152dd2e4f2d903b50b7e67c43482f62b78ea
Roberto Gabriel <120602524+rgabrielw@users.noreply.github.com>
2023-05-18 00:20:33 -0400

    Update cubo.h

--- 20230512_001/cubo.h
+++ 20230512_001/cubo.h
@@ -1,1 +1,13 @@
+#ifndef CUBO_H_INCLUDED
+#define CUBO_H_INCLUDED
 
+typedef struct cubo{
+	float lado;
+	float area;
+	float volume;
+}cubo;
+
+void recebe(cubo *valor);
+void imprime(cubo *valor);
+
+#endif //CUBO_H_INCLUDED
