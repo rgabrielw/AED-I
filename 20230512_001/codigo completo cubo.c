67e79414fae71dba6752e1fd8b0b439dae8c1c3d
Roberto Gabriel <120602524+rgabrielw@users.noreply.github.com>
2023-05-18 00:22:25 -0400

    Update codigo completo cubo.c

--- 20230512_001/codigo completo cubo.c
+++ 20230512_001/codigo completo cubo.c
@@ -1,3 +1,5 @@
+/*1. Desenvolva um TAD que represente um cubo. Inclua as funções de inicializações necessárias e as operações que retornem os tamanhos de cada lado, a sua área e o seu volume.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
